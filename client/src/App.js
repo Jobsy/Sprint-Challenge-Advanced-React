9d1ff0f780bd4fa9a5f4e8aa19379557ae564470
Oluwajoba <oluwajobabello@gmail.com>
2019-10-04 08:42:15 +0100

    install and react-router-dom / package.json / project setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import axios from "axios"
 import { Link, Route } from 'react-router-dom';
 
-import Users from './components/Users';
-import Followers from './components/Followers';
-import Navbar from "./components/NavBar";
+import Players from './components/Players';
+// import Followers from './components/Followers';
+// import Navbar from "./components/NavBar";
 
 // import logo from './logo.svg';
 import './App.css';
@@ -14,29 +14,35 @@ class App extends React.Component {
     super();
     this.state = {
       users: {},
-      followers: []
+      // followers: []
     };
 
   }
 
-  
+  componentDidMount() {
+    axios.get("https://api.github.com/users/Jobsy").then(response => {
+      console.log("LLL: ", response);
+      this.setState({ users: response.data });
+    });
+  }
 
   render() {
     return (
       <div className="app">
-              <Navbar />
+              {/* <Navbar /> */}
 
         <div>
           <Link exact to="/"><h1>GitHub User Card</h1></Link>
-          <Route exact path="/" render={(props) => <Users {...props} usersProps={this.state.users} />} />
+          <Route exact path="/" render={(props) => <Players {...props} usersProps={this.state.users} />} />
         </div>
 
-        <div>
+        {/* <div>
           <Route exact path="/followers" render={(props) => <Followers {...props} followersProps={this.state.followers} />} />
-        </div>
+        </div> */}
       </div>
     );
   }
 }
 
 export default App;
+// 
